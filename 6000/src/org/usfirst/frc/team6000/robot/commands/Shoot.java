b192de670715108fa7853c773a6868c3f693e1d2
Tyler Ekaireb <2018.t.ekaireb@shalhevet.org>
2017-01-22 12:49:00 -0800

    Test

---
+++ 6000/src/org/usfirst/frc/team6000/robot/commands/Shoot.java
@@ -0,0 +1,49 @@
+package org.usfirst.frc.team6000.robot.commands;
+
+import org.usfirst.frc.team6000.robot.Robot;
+import org.usfirst.frc.team6000.robot.subsystems.Shooter;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+
+/* Shoot Instructions:
+ * 
+ * Set shooter elevation
+ * Set wheel speed (to 0.6)
+ * Feed balls into shooter
+ */
+
+public class Shoot extends Command {
+
+    public Shoot() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.shooter);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.shooter.spinShooterWheel();
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
