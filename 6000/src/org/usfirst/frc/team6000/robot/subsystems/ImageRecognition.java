075286beccfb54a79232a01e84619a37cdfcf3c0
jacobf18 <minifeitelberg@att.net>
2017-01-25 00:31:40 -0500

    Update ImageRecognition.java

--- 6000/src/org/usfirst/frc/team6000/robot/subsystems/ImageRecognition.java
+++ 6000/src/org/usfirst/frc/team6000/robot/subsystems/ImageRecognition.java
@@ -15,9 +15,25 @@ public class ImageRecognition extends Subsystem {
 		
 	}
 	
-	// Checks if the robot is at a good enough angle to move straight to put the gear on the peg
-	// Requires: Ultrasonic sensor data
-	public void checkIfRightAngle() {
+	/* Checks if the robot is at a good enough angle to move straight to put the gear on the peg
+	   Requires: Ultrasonic sensor data
+	   Variables in use:
+	     d = distance from camera to wall that bisects the viewing angle of the camera
+	     w = half the width in the camcera frame
+	     angRaw = the angle between the viewing angle bisector and the flat wall
+	     angError = the difference between 90 degrees, which is perfect, and the angRaw
+	   To calculate angRaw:
+	     90:angRaw just as 2in:apparent width of retroflective tape
+	*/
+	public void checkIfRightAngle(float d) {
+		float w = 0; // in Pixels originally, then convert to inches
+		float angRaw = 90.0;
+		float angError = 90 - angRaw;
+		float maxError = 0;
+	}
+	
+	// converts a length in the image from pixels to inches
+	public void convertToInches (float pix){
 		
 	}
 
