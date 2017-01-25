bf1409cbd66453c6ebfd164ab729e9ed299e011f
jacobf18 <minifeitelberg@att.net>
2017-01-25 00:15:15 -0500

    Update ImageRecognition.java

--- 6000/src/org/usfirst/frc/team6000/robot/subsystems/ImageRecognition.java
+++ 6000/src/org/usfirst/frc/team6000/robot/subsystems/ImageRecognition.java
@@ -7,6 +7,19 @@ public class ImageRecognition extends Subsystem {
 	protected void initDefaultCommand() {
 		
 	}
+	
+	// For autonomous
+	
+	// Checks if the camera is looking at the vision targets in the gear peg area
+	public void checkIfGearTarget() {
+		
+	}
+	
+	// Checks if the robot is at a good enough angle to move straight to put the gear on the peg
+	// Requires: Ultrasonic sensor data
+	public void checkIfRightAngle() {
+		
+	}
 
 }
 
