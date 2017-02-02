d0eb893fe7bda7adf335b47841aae77a01113412
Tyler Ekaireb <2018.t.ekaireb@shalhevet.org>
2017-02-01 16:47:55 -0800

    Fixed Image Recognition

--- 6000/src/org/usfirst/frc/team6000/robot/subsystems/ImageRecognition.java
+++ 6000/src/org/usfirst/frc/team6000/robot/subsystems/ImageRecognition.java
@@ -27,13 +27,8 @@ public class ImageRecognition extends Subsystem {
 	  d is the width of the image in pixels.
 	  
 	  agnAlign = arctan((w/d)tan(30));
-<<<<<<< HEAD
-	*/
+*/
 	
-=======
-	  
-	*/
->>>>>>> branch 'master' of https://github.com/Team6000/FRC6000-2017
 	public void alignCenter() {
 		
 	}
@@ -50,29 +45,18 @@ public class ImageRecognition extends Subsystem {
 	   To calculate Apparent width of retroflective tape
 	     (FIND FORMULA TO CALCULATE W WHEN ANGRAW IS 90 DEGREES) inches : w in pixels just as apparent width : w in pixels.
 	*/
+	
 	public void checkIfRightAngle(float d) {
 		double w = 0; // in Pixels originally, then convert to inches
-<<<<<<< HEAD
 		double angRaw = 90.0;
 		double angError = 90 - angRaw;
 		double maxError = 0;
-=======
-		double angRaw = 90;
-		double angError = 90 - angRaw;
-		double maxError = 0;
 		
-		
->>>>>>> branch 'master' of https://github.com/Team6000/FRC6000-2017
 	}
 	
 	// converts a length in the image from pixels to inches
-<<<<<<< HEAD
 	public void convertToInches (float pix){
 		// return pixels divided by DPI (dots per inch)
-=======
-	public void convertToInches (double pix){
-		
->>>>>>> branch 'master' of https://github.com/Team6000/FRC6000-2017
 	}
 
 }
