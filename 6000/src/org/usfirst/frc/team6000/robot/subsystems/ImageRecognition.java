ec10d2d369ab6c479a3cc19faf651aba6f176213
jacobf18 <minifeitelberg@att.net>
2017-02-09 00:07:40 -0800

    Added Image Rec stuff.  If this messes everything up, I'll revert it

--- 6000/src/org/usfirst/frc/team6000/robot/subsystems/ImageRecognition.java
+++ 6000/src/org/usfirst/frc/team6000/robot/subsystems/ImageRecognition.java
@@ -1,7 +1,7 @@
 package org.usfirst.frc.team6000.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
-import java.awt.*;
+import java.awt.Point;
 
 public class ImageRecognition extends Subsystem {
 
