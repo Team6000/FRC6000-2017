a19182a1a7d322da782ccc72f000f9b348b5b20b
jacobf18 <minifeitelberg@att.net>
2017-02-13 19:30:54 -0800

    Finished the processing of the lines to get P1-P8

--- 6000/src/org/usfirst/frc/team6000/robot/subsystems/CameraData.java
+++ 6000/src/org/usfirst/frc/team6000/robot/subsystems/CameraData.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team6000.robot.subsystems;
 import java.awt.Point;
 import org.usfirst.frc.team6000.robot.subsystems.Line;
+import java.util.*;
 
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
@@ -52,6 +53,22 @@ public class CameraData {
 	
 	double[] defaultValue = new double[0];
 	
+	List<Line> checkLines = new ArrayList<>();
+	
+	double max1 = 0;
+	double max2 = 0;
+	double max3 = 0;
+	double max4 = 0;
+	double min1 = 0;
+	double min2 = 0;
+	double min3 = 0;
+	double min4 = 0;
+	
+	double lineX1 = 0;
+	double lineX2 = 0;
+	double lineX3 = 0;
+	double lineX4 = 0;
+	
 	public void popLines(){
 		while(true){
 			Line l = new Line();
@@ -80,11 +97,95 @@ public class CameraData {
 				break;
 			}
 			else if (l.alignment == 'v'){
-				
+				checkLines.add(l);
 			}
 			else{
 				System.out.println("The line is not set up correctly");
 				break;
+			}
+		}
+	}
+	
+	public void segregateLines(){
+		double cursor = 0;
+		double lineNumber = 0;
+		double leeWay = 15;
+		for(int i = 0; i<checkLines.size(); i++){
+			cursor = checkLines.get(i).x1;
+			// Go through every single vertical line to see if it has three ahead of it
+			cursor += leeWay;
+			for(int j = 0; j<1280-checkLines.get(i).x1; j++){
+				cursor ++;
+				for (int k = 0; k<checkLines.size(); k++){
+					if(cursor == checkLines.get(k).x1){
+						lineNumber ++;
+						cursor += leeWay;
+						break;
+					}
+				}
+			}
+			checkLines.get(i).bigLine = lineNumber;
+		}
+	}
+	
+	public void cutMedians(){
+		double max = 0;
+		double min = 720;
+		double lineX = 0;
+		for (int i = 1; i<5; i++){
+			
+			for (int j = 0; j<checkLines.size(); j++){
+				// if the lines being checked are in the same big line
+				if(checkLines.get(j).bigLine == i){
+					// set the max number to be the highest lines top point y value
+					if(checkLines.get(j).y1 > max || checkLines.get(j).y2 > max)
+					{
+						if(checkLines.get(j).y1 > checkLines.get(j).y2){
+							lineX = checkLines.get(j).x1;
+							max = checkLines.get(j).y1;
+						}
+						else{
+							lineX = checkLines.get(j).x2;
+							max = checkLines.get(j).y2;
+						}
+					}
+					
+					if(checkLines.get(j).y1 < min || checkLines.get(j).y2 < min){
+						if(checkLines.get(j).y1 < checkLines.get(j).y2){
+							lineX = checkLines.get(j).x1;
+							min = checkLines.get(j).y1;
+						}
+						else{
+							lineX = checkLines.get(j).x2;
+							min = checkLines.get(j).y2;
+						}
+					}
+				}
+			}
+			// assign the values found to the global variables for P1-P8;
+			switch(i){
+				case 1:
+					lineX1 = lineX;
+					max1=max;
+					min1=min;
+					break;
+				case 2:
+					lineX2 = lineX;
+					max2=max;
+					min2=min;
+					break;
+				case 3:
+					lineX3 = lineX;
+					max3=max;
+					min3=min;
+					break;
+				case 4:
+					lineX4 = lineX;
+					max4=max;
+					min4=min;
+					break;
+				default:
+					break;
 			}
 		}
 	}
@@ -113,38 +214,57 @@ public class CameraData {
 	 */
 	
 	public Point getP1(){
-		return null;
+		Point P1 = new Point();
+		P1.x = (int) lineX1;
+		P1.y = (int) max1;
+		return P1;
 	}
 	public Point getP2(){
-		return null;
+		Point P2 = new Point();
+		P2.x = (int) lineX2;
+		P2.y = (int) max2;
+		return P2;
 	}
 	public Point getP3(){
-		return null;
+		Point P3 = new Point();
+		P3.x = (int) lineX1;
+		P3.y = (int) min1;
+		return P3;
 	}
 	public Point getP4(){
-		return null;
+		Point P4 = new Point();
+		P4.x = (int) lineX2;
+		P4.y = (int) min2;
+		return P4;
 	}
 	public Point getP5(){
-		return null;
-		
+		Point P5 = new Point();
+		P5.x = (int) lineX3;
+		P5.y = (int) max3;
+		return P5;
 	}
 	public Point getP6(){
-		return null;
-		
+		Point P6 = new Point();
+		P6.x = (int) lineX4;
+		P6.y = (int) max4;
+		return P6;
 	}
 	public Point getP7(){
-		return null;
+		Point P7 = new Point();
+		P7.x = (int) lineX3;
+		P7.y = (int) min3;
+		return P7;
 		
 	}
 	public Point getP8(){
-		return null;
+		Point P8 = new Point();
+		P8.x = (int) lineX4;
+		P8.y = (int) min4;
+		return P8;
 		
-	}
-	public int camWidth(){
-		return 0;
 	}
 
 	public int CAM_WIDTH(){
-		return 0;
+		return 1280;
 	}
 }
--- 6000/src/org/usfirst/frc/team6000/robot/subsystems/Line.java
+++ 6000/src/org/usfirst/frc/team6000/robot/subsystems/Line.java
@@ -1,8 +1,6 @@
 package org.usfirst.frc.team6000.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
-import org.usfirst.frc.team6000.robot.Robot;
-import org.usfirst.frc.team6000.robot.RobotMap;
 
 public class Line extends Subsystem {
 
